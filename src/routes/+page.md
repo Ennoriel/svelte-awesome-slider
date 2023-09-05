6889ff725257e531a888dfbdb8177431c9378eed
Maxime Dupont <m.dupont103@gmail.com>
2023-09-05 05:25:37 +0200

    Add mdsvex + vercel config

---
+++ src/routes/+page.md
@@ -0,0 +1,5 @@
+# Welcome to your library project
+
+Create your package using @sveltejs/package and preview/showcase your work with SvelteKit
+
+Visit [kit.svelte.dev](https://kit.svelte.dev) to read the documentation
--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,22 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import preprocess from 'svelte-preprocess';
+import { mdsvex } from 'mdsvex';
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import vercel from '@sveltejs/adapter-vercel';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+	preprocess: [
+		preprocess(),
+		mdsvex({
+			extensions: ['.md'],
+			rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings]
+		})
+	],
+
+	extensions: ['.svelte', '.md'],
 
 	kit: {
-		adapter: adapter()
+		adapter: vercel()
 	},
 
 	vitePlugin: {
