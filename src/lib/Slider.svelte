3ef6a7aea65837b7f28af26892350c848c417141
Maxime Dupont <m.dupont103@gmail.com>
2023-09-22 21:50:51 +0200

    tidy up config + fix bugs in Slider

---
+++ src/lib/Slider.svelte
@@ -0,0 +1,245 @@
+<script lang="ts">
+	export let min: string | number = 0;
+	$: _min = typeof min === 'string' ? parseFloat(min) : min;
+
+	export let max: string | number = 100;
+	$: _max = typeof max === 'string' ? parseFloat(max) : max;
+
+	export let step: string | number = 1;
+	$: _step = typeof step === 'string' ? parseFloat(step) : step;
+
+	export let value: number = 50;
+	export let ariaValueText: (current: number) => string = (current) => current.toString();
+	export let name: string | undefined = undefined;
+	export let direction: 'horizontal' | 'vertical' = 'horizontal';
+	export let reverse = false;
+
+	export let keyboardOnly = false;
+
+	let isDragging = false;
+	let slider: HTMLDivElement | undefined = undefined;
+
+	function bound(value: number) {
+		const ratio = 1 / _step;
+		const rounded = Math.round(value * ratio) / ratio;
+		return Math.max(_min, Math.min(_max, rounded));
+	}
+
+	function keyHandler(e: KeyboardEvent) {
+		const inc = e.shiftKey ? _step * 10 : _step;
+		if (e.key === 'ArrowUp' || e.key === 'ArrowRight') {
+			value += inc;
+		} else if (e.key === 'ArrowDown' || e.key === 'ArrowLeft') {
+			value -= inc;
+		} else if (e.key === 'Home') {
+			value = _min;
+		} else if (e.key === 'End') {
+			value = _max;
+		} else if (e.key === 'PageUp') {
+			value += _step * 10;
+		} else if (e.key === 'PageDown') {
+			value -= _step * 10;
+		}
+		value = bound(value);
+	}
+
+	const a = {
+		horizontal: {
+			clientSize: 'clientWidth',
+			offset: 'left',
+			client: 'clientX'
+		},
+		vertical: {
+			clientSize: 'clientHeight',
+			offset: 'top',
+			client: 'clientY'
+		}
+	} as const;
+
+	function updateValue(e: { clientX: number; clientY: number }) {
+		const clientWidth = slider?.[a[direction].clientSize] || 120;
+		const sliderOffsetX = slider?.getBoundingClientRect()[a[direction].offset] || 0;
+
+		let offsetX = e[a[direction].client] - sliderOffsetX;
+		if (direction === 'vertical') offsetX = -1 * offsetX + clientWidth;
+
+		if (reverse) {
+			value = _max - (offsetX / clientWidth) * (_max - _min);
+		} else {
+			value = (offsetX / clientWidth) * (_max - _min) + _min;
+		}
+
+		value = bound(value);
+	}
+
+	function jump(e: MouseEvent) {
+		updateValue(e);
+		isDragging = true;
+	}
+
+	function drag(e: MouseEvent) {
+		if (isDragging) updateValue(e);
+	}
+
+	function endDrag() {
+		isDragging = false;
+	}
+
+	function touch(e: TouchEvent) {
+		e.preventDefault();
+		updateValue({
+			clientX: e.changedTouches[0].clientX,
+			clientY: e.changedTouches[0].clientY
+		});
+	}
+
+	$: position = (((value - _min) / (_max - _min)) * 1).toFixed(4);
+</script>
+
+<svelte:window on:mousemove={drag} on:mouseup={endDrag} />
+
+<div
+	class="slider"
+	class:reverse
+	role="slider"
+	aria-orientation={direction}
+	aria-valuemax={_max}
+	aria-valuemin={_min}
+	aria-valuenow={value}
+	aria-valuetext={ariaValueText(value)}
+	tabindex="0"
+	bind:this={slider}
+	style:--position={position}
+	on:keydown={keyHandler}
+	on:mousedown|self={keyboardOnly ? undefined : jump}
+	on:touchstart={keyboardOnly ? undefined : touch}
+	on:touchmove={keyboardOnly ? undefined : touch}
+	on:touchend={keyboardOnly ? undefined : touch}
+>
+	<div class="track" />
+	<div class="thumb" />
+</div>
+
+{#if name}
+	<input type="hidden" {name} {value} />
+{/if}
+
+<style>
+	.slider {
+		---track-width: var(--track-width, 120px);
+		---track-height: var(--track-height, 10px);
+		---track-background: var(--track-background, #666);
+		---track-border: var(--track-border, 1px solid black);
+		---thumb-size: var(--thumb-size, 20px);
+		---thumb-background: var(--thumb-background, #777);
+		---thumb-border: var(--thumb-border, 1px solid black);
+		---position: var(--position, 0px);
+
+		---margin-inline-thumb-bigger: max(var(---thumb-size) - var(---track-height), 0px);
+		---margin-inline-thumb-smaller: max(var(---track-height) - var(---thumb-size), 0px);
+
+		position: relative;
+		margin: auto;
+		user-select: none;
+		-webkit-user-select: none;
+	}
+
+	[aria-orientation='horizontal'] {
+		width: var(---track-width);
+		max-width: calc(100% - 2 * var(---margin-inline-thumb-bigger));
+		height: calc(max(var(---track-height), var(---thumb-size)) + 4px);
+		height: max(var(---track-height), var(---thumb-size));
+		margin-inline: var(---margin-inline-thumb-bigger);
+	}
+
+	[aria-orientation='vertical'] {
+		width: calc(max(var(---track-height), var(---thumb-size)) + 4px);
+		width: max(var(---track-height), var(---thumb-size));
+		height: var(---track-width);
+		max-height: calc(100% - 2 * var(---margin-inline-thumb-bigger));
+		margin-block: var(---margin-inline-thumb-bigger);
+	}
+
+	.track {
+		position: absolute;
+		pointer-events: none;
+		background: var(---track-background);
+		border: var(---track-border);
+		border-radius: calc(var(---track-height) / 2);
+		box-sizing: border-box;
+	}
+
+	[aria-orientation='horizontal'] .track {
+		height: var(---track-height);
+		top: 50%;
+		transform: translateY(-50%);
+		left: 0;
+		right: 0;
+	}
+
+	[aria-orientation='vertical'] .track {
+		width: var(---track-height);
+		left: 50%;
+		transform: translateX(-50%);
+		top: 0;
+		bottom: 0;
+	}
+
+	.thumb {
+		pointer-events: none;
+		position: absolute;
+		height: var(--thumb-size, 20px);
+		width: var(--thumb-size, 20px);
+		border-radius: calc(var(---thumb-size) / 2);
+		background: var(---thumb-background);
+		border: var(---thumb-border);
+		box-sizing: border-box;
+
+		transform: translate(-50%, -50%);
+		--left-margin: (
+				2 * var(---track-height) - var(---thumb-size) - var(---margin-inline-thumb-smaller)
+			) / 2;
+		--left: calc(var(---position) * (100% - 2 * var(--left-margin)) + var(--left-margin));
+	}
+
+	[aria-orientation='horizontal']:not(.reverse) .thumb {
+		top: 50%;
+		left: var(--left);
+	}
+
+	[aria-orientation='vertical']:not(.reverse) .thumb {
+		left: 50%;
+		bottom: calc(var(--left) - var(---thumb-size));
+	}
+
+	[aria-orientation='horizontal'].reverse .thumb {
+		top: 50%;
+		right: calc(var(--left) - var(---thumb-size));
+	}
+
+	[aria-orientation='vertical'].reverse .thumb {
+		left: 50%;
+		top: calc(var(--left));
+	}
+
+	.slider:focus-visible {
+		outline: none;
+	}
+
+	.slider:focus-visible .track {
+		outline: 2px solid var(--focus-color, red);
+		outline-offset: 2px;
+	}
+
+	.slider:active,
+	.track:active,
+	.thumb:active {
+		cursor: ew-resize;
+	}
+
+	[aria-orientation='vertical'].slider:active,
+	[aria-orientation='vertical'] .track:active,
+	[aria-orientation='vertical'] .thumb:active {
+		cursor: ns-resize;
+	}
+</style>
--- src/routes/+page.md
+++ src/routes/+page.md
@@ -1,5 +1,126 @@
+<script>
+	import Slider from "$lib/Slider.svelte"
+
+	let value = 50
+	let hue = 0
+
+    const str = `
+			linear-gradient(to right, rgba(0,0,0,0), rgba(255,0,0,1)),
+			linear-gradient(45deg, #eee 25%, transparent 25%, transparent 75%, #eee 75%) top left / 12px,
+            linear-gradient(45deg, #eee 25%, transparent 25%, transparent 75%, #eee 75%) 6px 6px / 12px`;
+</script>
+
 # Welcome to your library project
 
 Create your package using @sveltejs/package and preview/showcase your work with SvelteKit
 
 Visit [kit.svelte.dev](https://kit.svelte.dev) to read the documentation
+
+<Slider
+min={0}
+max={360}
+step={5}
+value={0}
+--track-background="#eee"
+--thumb-background="transparent"
+/>
+
+<Slider
+min={0}
+max={360}
+step={5}
+value={0}
+--track-width="360px"
+--track-height="24px"
+--track-background="#eee"
+--thumb-size="14px"
+--thumb-background="transparent"
+/>
+
+<Slider
+min={0}
+max={360}
+step={5}
+value={360}
+--track-width="3600px"
+--track-background="#eee"
+--thumb-background="transparent"
+/>
+
+<Slider
+min={0}
+max={360}
+step={5}
+value={360}
+--track-width="3600px"
+--track-height="24px"
+--track-background="#eee"
+--thumb-size="14px"
+--thumb-background="transparent"
+/>
+
+<Slider
+min={0}
+max={360}
+step={5}
+value={360}
+--track-width="3600px"
+--track-height="24px"
+--track-background="#eee"
+--thumb-size="24px"
+--thumb-background="transparent"
+/>
+
+<div style:display="flex">
+
+<Slider
+min={0}
+max={360}
+step={5}
+value={0}
+direction="vertical"
+--track-width="360px"
+--track-height="24px"
+--track-background="#eee"
+--thumb-size="24px"
+--thumb-background="transparent"
+/>
+
+<Slider
+min={0}
+max={360}
+step={5}
+value={360}
+direction="vertical"
+--track-width="360px"
+--track-height="24px"
+--track-background="#eee"
+--thumb-size="24px"
+--thumb-background="transparent"
+/>
+
+</div>
+
+<!--
+--track-width
+--track-height
+--track-background
+--thumb-size
+--thumb-background
+--position
+-->
+
+## bugs
+
+- [x] focus style
+- [x] thumb position
+- [x] value when dragging not over
+- [x] thumb position
+- [x] touch event
+- [x] vertical layout
+- [ ] documentation
+- [ ] customization?
+- [x] vertical slider
+- [x] copy color
+- [ ] margins
+- [ ] build size
